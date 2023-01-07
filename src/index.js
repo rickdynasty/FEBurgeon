face9b04e7e35bf8f3b8273fdba5b562a874ec20
shangyong.chen <shangyong.chen@shopee.com>
2023-01-07 08:36:52 +0800

    init burgeon

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// 先导入 antd 样式文件
+// https://github.com/ant-design/ant-design/issues/33327
+import 'antd/dist/antd.min.css'
+// 再导入全局样式文件，防止样式覆盖！
+import './index.scss'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
